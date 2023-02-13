53366baf89c403371ddac75dd25b90c40162f8ba
SergiuszRozycki <sergiuszrozycki@icloud.com>
2023-02-13 12:55:07 +0100

    brakepints

--- main.js
+++ main.js
@@ -34,6 +34,23 @@ const swiper = new Swiper("#swiper", {
     enabled: true,
   },
   spaceBetween: 20,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
 
   // If we need pagination
   pagination: {
@@ -55,6 +72,23 @@ const swiper2 = new Swiper("#swiper-2", {
     enabled: true,
   },
   spaceBetween: 20,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
 
   // If we need pagination
   pagination: {
@@ -64,6 +98,6 @@ const swiper2 = new Swiper("#swiper-2", {
   // Navigation arrows
   navigation: {
     nextEl: "#is-right-2",
-    prevEl: "#is-left-2",
+    prevEl: "#is-left",
   },
 });
