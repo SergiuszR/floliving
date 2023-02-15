915a57a58c1a80a60b608ac742d70d18c06075cf
SergiuszRozycki <sergiuszrozycki@icloud.com>
2023-02-15 10:44:58 +0100

    update

--- main.js
+++ main.js
@@ -2,98 +2,100 @@
 let faqWrapper = document.querySelector(".faq3_list");
 let chevron = `https://uploads-ssl.webflow.com/63bfe07d48817929da122d0d/63c555d34a9d3a4234b82473_chevron.svg`;
 
-fetch(
-  "https://floliving.zendesk.com/api/v2/help_center/articles.json?sort_by=updated_at&sort_order=aschttps://floliving.zendesk.com/api/v2/help_center/articles.json?sort_by=updated_at&sort_order=asc",
-  {
-    headers: {
-      Accept: "*/*",
+// fetch(
+//   "https://floliving.zendesk.com/api/v2/help_center/articles.json?sort_by=updated_at&sort_order=aschttps://floliving.zendesk.com/api/v2/help_center/articles.json?sort_by=updated_at&sort_order=asc",
+//   {
+//     headers: {
+//       Accept: "*/*",
+//     },
+//     method: "GET",
+//   }
+// )
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     let articles = data.articles;
+
+//     articles.forEach((article) => {
+//       let accordionItem = document.createElement("div");
+//       accordionItem.classList.add("faq3_accordion");
+//       accordionItem.innerHTML = `<div class="faq3_question"><div class="size-4">${article.title}</div><img src="${chevron}" loading="lazy" alt="" class="faq3_icon" style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(180deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div><div style="width: 100%;" class="faq3_answer"><div class="margin-bottom--s"><p class="no-margin-bottom">${article.body}</div></div>`;
+//       faqWrapper.append(accordionItem);
+//     });
+//   });
+
+try {
+  const swiper = new Swiper("#swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    autoplay: {
+      delay: 3000,
     },
-    method: "GET",
-  }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then((data) => {
-    let articles = data.articles;
+    slidesPerView: "auto",
+    keyboard: {
+      enabled: true,
+    },
+    spaceBetween: 20,
 
-    articles.forEach((article) => {
-      let accordionItem = document.createElement("div");
-      accordionItem.classList.add("faq3_accordion");
-      accordionItem.innerHTML = `<div class="faq3_question"><div class="size-4">${article.title}</div><img src="${chevron}" loading="lazy" alt="" class="faq3_icon" style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(180deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div><div style="width: 100%;" class="faq3_answer"><div class="margin-bottom--s"><p class="no-margin-bottom">${article.body}</div></div>`;
-      faqWrapper.append(accordionItem);
-    });
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: "#is-right",
+      prevEl: "#is-left",
+    },
   });
+  const swiper2 = new Swiper("#swiper-2", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: "auto",
+    autoplay: {
+      delay: 3000,
+    },
+    keyboard: {
+      enabled: true,
+    },
+    spaceBetween: 20,
 
-const swiper = new Swiper("#swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  autoplay: {
-    delay: 3000,
-  },
-  slidesPerView: "auto",
-  keyboard: {
-    enabled: true,
-  },
-  spaceBetween: 20,
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: "#is-right-2",
+      prevEl: "#is-left-2",
+    },
+  });
+  const swiper3 = new Swiper("#swiper-3", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: "auto",
+    autoplay: {
+      delay: 3000,
+    },
+    keyboard: {
+      enabled: true,
+    },
+    spaceBetween: 20,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: "#is-right",
-    prevEl: "#is-left",
-  },
-});
-const swiper2 = new Swiper("#swiper-2", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  slidesPerView: "auto",
-  autoplay: {
-    delay: 3000,
-  },
-  keyboard: {
-    enabled: true,
-  },
-  spaceBetween: 20,
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: "#is-right-2",
-    prevEl: "#is-left-2",
-  },
-});
-const swiper3 = new Swiper("#swiper-3", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  slidesPerView: "auto",
-  autoplay: {
-    delay: 3000,
-  },
-  keyboard: {
-    enabled: true,
-  },
-  spaceBetween: 20,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: "#is-right-2",
-    prevEl: "#is-left-2",
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: "#is-right-2",
+      prevEl: "#is-left-2",
+    },
+  });
+} catch (err) {}
